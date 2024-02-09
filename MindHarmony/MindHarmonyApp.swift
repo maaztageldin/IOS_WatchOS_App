6739797a02eb28a6218daa6fb4bc8a8800ac710e
tageldin <maaztageldin030@hotmail.com>
2024-02-09 17:17:00 +0100

    Initial Commit

---
+++ MindHarmony/MindHarmonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindHarmonyApp.swift
+//  MindHarmony
+//
+//  Created by Maaz TAGELDIN on 09/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MindHarmonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
